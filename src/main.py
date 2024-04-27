c6376ea0a6a0b5883ad993ffc44630ce0079e95c
Rithik2211 <rithikpranao22@gmail.com>
2024-04-27 12:06:56 +0530

    Update route handlers in main.py; add function and list iteration in test.py
    The changes in main.py include updating the route handler for the root path to accept a name parameter and modifying the about route handler to include a

--- src/main.py
+++ src/main.py
@@ -3,9 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/Home')
-def hello_world():
-    return 'Hello, Welcome to the flask project!!'
+@app.route('/Home/<name>')
+def hello_world(name):
+    return 'Hello, Welcome to {}'.format(name)
+
+@app.route('/about/<name>')
+def about(name):
+    return 'Welcome to the {about} page {name}'.format(name=name, about="grand")
+
+@app.route('/profile')
+def profile():
+    return 'This is the profile page'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/test.py
+++ src/test.py
@@ -3,6 +3,11 @@ data = f"this is a common string of data{a}"
 print(data)
 
 def fun(d):
-    print(d);
+    print(d)
 
 fun("hello this is a function")
+
+e = ["apple","mango","cherry","papaya"]
+
+for i in e :
+    print(i)
