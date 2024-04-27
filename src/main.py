8fc4972f457768378b016b46c686bb30ae246745
Rithik2211 <rithikpranao22@gmail.com>
2024-04-27 22:00:37 +0530

    added render templated and worked on with the fask basic page  renderings

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/Home/<name>')
-def hello_world(name):
-    return 'Hello, Welcome to {}'.format(name)
+@app.route('/Home')
+def hello_world():
+    return 'Hello, Welcome'
 
 @app.route('/about/<name>')
 def about(name):
-    return 'Welcome to the {about} page {name}'.format(name=name, about="grand")
+    array = ["apple","mango","orange","cherry"]
+    return render_template("about.html", user_name = name,  data = array, isloggedIn = True)
 
 @app.route('/profile')
 def profile():
--- src/test.py
+++ src/test.py
@@ -1,13 +1,3 @@
-a = 12
-data = f"this is a common string of data{a}"
-print(data)
-
-def fun(d):
-    print(d)
-
-fun("hello this is a function")
-
 e = ["apple","mango","cherry","papaya"]
 
-for i in e :
-    print(i)
+[print(i) for i in e]
