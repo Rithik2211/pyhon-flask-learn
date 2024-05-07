e620f64252b03b8f106c2838bd0909f993ea3146
Rithik2211 <rithikpranao22@gmail.com>
2024-05-07 07:57:25 +0530

    today formula

---
+++ dsa/formula.py
@@ -0,0 +1,10 @@
+'''
+ 1) print sum of all the numbers in the given series :  n*n+1 // 2
+
+ 2) print celcius to fahrenheit :   (c * 9/5) + 32     # 32 degree offset
+
+ 3) print fahrenheit to celcius :  (f - 32) * 5/9       # 32 degree offset
+
+ 4) count length of the numbers : math.floor(math.log10(N)+1)     # import math | T-complexity -> O(1)
+
+'''
