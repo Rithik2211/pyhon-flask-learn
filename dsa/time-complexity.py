63179f38226b4dee691d17363187c9ffd7d9a3d5
Rithik2211 <rithikpranao22@gmail.com>
2024-05-13 22:51:07 +0530

    time complexities

--- dsa/time-complexity.py
+++ dsa/time-complexity.py
@@ -2,6 +2,17 @@
 Asymptotic analysis of an Program :
  - multiplicaition, division, addition are constants operators they are not do n times
 
+ Omega Notation : 
+ - It represents the best case scenario of the algorithm
+
+ Theta Notation : 
+ - It represents the Upper and lower bound in the running of an algorithm
+ - It gives the average complexity of the algorithm.
+
+ Big O Notation :
+ - It is useful when it having the Upper bound of the time complexity of the application.
+ - It gives the worst case time complexity of the algorithm.
+
 
 
 '''
