cefbee7bcb87a84d227093814c6dcc5fe97a2116
Rithik2211 <rithikpranao22@gmail.com>
2024-05-23 21:11:32 +0530

    trailing zeros

--- dsa/formula.py
+++ dsa/formula.py
@@ -18,4 +18,17 @@
             digit += math.log10(i)
         return math.floor(digit +1)
 
+ 7) Find the Trailing of the zeros :
+    def findTrailingZeros(n):
+        count = 0
+        # Keep dividing n by powers of 5 and update Count
+        i = 5
+        while (n / i>= 1):
+            count += int(n / i)
+            i *= 5
+
+        return int(count)
+    
+ 
+
 '''
