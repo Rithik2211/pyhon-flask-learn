a7f53be75eefd6467cbe4a2047441999920b232b
Rithik2211 <rithikpranao22@gmail.com>
2024-06-04 19:49:37 +0530

    lcm of numbers

--- dsa/formula.py
+++ dsa/formula.py
@@ -34,7 +34,18 @@
         if b==0:
             return a
         return gcd(b,a%b)
-        
- 
 
+ 
+9) An efficient solution is based on the below formula for LCM of two numbers ‘a’ and ‘b’. 
+   a x b = LCM(a, b) * GCD (a, b)
+   LCM(a, b) = (a x b) / GCD(a, b)
+   solution : 
+
+    def gcd(a,b):
+       if a==0:
+        return b
+       return gcd(b%a, a)
+    def lcm(a,b):
+        return (a/gcd(a,b))*b
+    
 '''
