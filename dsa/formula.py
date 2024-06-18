a8cf854dab8918a45d77c5c3a3d33397e7337e81
Rithik2211 <rithikpranao22@gmail.com>
2024-06-18 09:07:39 +0530

    prime factor

--- dsa/formula.py
+++ dsa/formula.py
@@ -88,6 +88,29 @@
             i +=6
         return True 
 
-11) 
+11) Prime Factor of number :
+
+def isPrime(n):
+    if n==1:
+        return False
+    if n==2 or n==3:
+        return True
+    if n%2==0 or n%3==0:
+        return False
+    i=5
+    while (i*i <= n):
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i+=6
+    return True
+
+a = []
+def primeFactor(n):
+    for i in range(2,n+1):
+        if isPrime(i):
+            x = i
+            while n%x == 0:
+                a.append(i)
+                x *= i
     
 '''
--- dsa/index.py
+++ dsa/index.py
@@ -1,40 +1,25 @@
-def gcd(a,b):
-    if b == 0:
-        return a
-    return gcd(b, a%b)
-def lcm(a,b):
-    return a * b // gcd(a,b)
-
 def isPrime(n):
-    if n ==1 :
-        return False
-    for i in range(2, n):
-        if n%i == 0:
-            return False
-    return True
-
-def checkPrime(n):
-    if n == 1:
-        return False
-    i = 2
-    while (i*i <= n):
-        if n%i == 0:
-            return False
-        i += 1
-    return True
-
-def primeSuperEff(n):
     if n==1:
         return False
     if n==2 or n==3:
         return True
-    if n%2 == 0 or n%3 == 0:
+    if n%2==0 or n%3==0:
         return False
-    i = 5
-    while(i*i <= n):
-        if (n%i == 0 or n % (i+2) == 0):
+    i=5
+    while (i*i <= n):
+        if n%i==0 or n%(i+2)==0:
             return False
-        i +=6
-    return True    
+        i+=6
+    return True
 
-print(primeSuperEff(1031))
+a = []
+def primeFactor(n):
+    for i in range(2,n+1):
+        if isPrime(i):
+            x = i
+            while n%x == 0:
+                a.append(i)
+                x *= i
+
+primeFactor(100)
+print(a)
