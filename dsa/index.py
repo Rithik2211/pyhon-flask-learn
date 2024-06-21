a632731a8af73a7cd127b8ad09da152cef021928
Rithik2211 <rithikpranao22@gmail.com>
2024-06-21 22:59:41 +0530

    linked list

--- dsa/index.py
+++ dsa/index.py
@@ -1,13 +1,1 @@
-def printDivisors(n):
-    i=1
-    while((i*i) < n):  # divisors from 1 to root n excuded
-        if(n%i == 0) :
-            print(i)
-        i+=1
-    while (i >= i): # divisors from root n includes from 1
-        if(n%i == 0):
-            print(n//i)
-        i-=1
-
-
-printDivisors(25)
+# linked list 
