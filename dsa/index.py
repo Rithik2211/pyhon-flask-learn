55ee6bf05f7121d3f18f7b646bc9700e9f315274
Rithik2211 <rithikpranao22@gmail.com>
2024-06-26 22:45:39 +0530

    soe problem

--- dsa/index.py
+++ dsa/index.py
@@ -1,15 +1,25 @@
+# stieve of erasthoensis
+
 def isPrime(n):
     if n == 1:
         return False
-    if n==2 or n==3:
+    if n ==2 or n==3:
         return True
     if n%2==0 or n%3==0:
         return False
-    i = 5
-    while(i*i <= n):
-        if(n%i ==0 or n%(i+2)==0):
+    i=5
+    while (i*i <= n):
+        if (n%i == 0 or n%(i+2)==0):
             return False
         i+=6
     return True
 
-print(isPrime(3))
+def soe(num):
+    a = []
+    for i in range(2, num+1):
+        if isPrime(i):
+            a.append(i)
+
+    return a
+
+print(soe(23))
